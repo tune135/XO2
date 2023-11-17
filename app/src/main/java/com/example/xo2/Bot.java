3edaaf4b84485c1ce789a918af399d6cc488f366
HP <evyatarkd@gmail.com>
2023-11-17 10:39:50 +0200

    Logit for bot done

--- app/src/main/java/com/example/xo2/Bot.java
+++ app/src/main/java/com/example/xo2/Bot.java
@@ -1,131 +1,362 @@
 package com.example.xo2;
 
+//The Bot class represents the logic for an AI player in the ultimate tic-tac-toe game.
 public class Bot {
 
-    private int[][] bigBoard;
-    private int[][][][] smallBoard;
-    private Boolean[][] playPossibility; // Where the bot canceled the play
-    private int play; // Where the bot plays
+    private GameBoard gb; //Game Board class
+    private int[][] playPossibility; // Where the bot canceled the play
+    private int botPlay; // Where the bot plays
+    private int botNum; //if bot plays X it will get 1 if bot plays O it will get -1
 
-    public Bot(int[][] bigBoard, int[][][][] smallBoard) {
-        this.bigBoard = bigBoard;
-        this.smallBoard = smallBoard;
-        playPossibility = new Boolean[3][3];
+
+    /**
+     * Constructor for the Bot class.
+     *
+     * @param gb     The GameBoard instance representing the game state.
+     * @param botNum The player number assigned to the bot (1 for X, -1 for O).
+     */
+    public Bot(GameBoard gb, int botNum) {
+        this.gb = gb;
+        this.botNum = botNum;
+        playPossibility = new int[3][3];
+        // Initialize playPossibility
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                this.playPossibility[i][j] = true;
+                this.playPossibility[i][j] = 0;
             }
         }
     }
 
-    public int[][] getBigBoard() {
-        return bigBoard;
+
+    /**
+     * Sets the player number for the bot (1 for X, -1 for O).
+     *
+     * @param botNum The player number to set for the bot.
+     */
+    public void setBotNum(int botNum) {
+        this.botNum = botNum;
     }
 
-    public void setBigBoard(int[][] bigBoard) {
-        this.bigBoard = bigBoard;
+    /**
+     * Gets the last chosen move by the bot.
+     *
+     * @return The last move chosen by the bot.
+     */
+    public int getBotPlay() {
+        return botPlay;
     }
 
-    public void setSmallBoard(int[][][][] smallBoard) {
-        this.smallBoard = smallBoard;
+    /**
+     * Sets the last chosen move by the bot.
+     *
+     * @param botPlay The move to set as the last chosen move by the bot.
+     */
+    public void setBotPlay(int botPlay) {
+        this.botPlay = botPlay;
     }
 
-    public int[][][][] getSmallBoard() {
-        return smallBoard;
+    /**
+     * Gets the current game board associated with the bot.
+     *
+     * @return The current game board.
+     */
+    public GameBoard getGb() {
+        return gb;
     }
 
-    public int playBot() {
+    /**
+     * Sets the game board for the bot.
+     *
+     * @param gb The game board to set for the bot.
+     */
+    public void setGb(GameBoard gb) {
+        this.gb = gb;
+    }
+
+
+    public int playBot(int boardPlay) {
         // Initialize play
+        if (boardPlay == -1) { //-1 for any play
+            return 0; //need to change
+        } else {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    this.playPossibility[i][j] = this.gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][i][j];
+                }
+            }
+            this.botPlay = -1;
+
+            while (canWinSBoard(boardPlay)) {
+                // Copy the small board state to playPossibility
+                if (canWinBBoard()) {
+                    return this.botPlay;
+                } else if (otherPlayerWinSBoardNextTurn()) {
+                    // Mark the position as not allowing the opponent to win
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 100; //Random number that isnt 1, -1, or 0 to show no possible win in that place
+                } else if (otherPlayerCanPlayAnywhere()) {
+                    // Mark the position as available for the opponent to play
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 10;//Random number that isnt 1, -1, or 0 to show no possible win in that place
+                } else {                                                            //the numbers are diffrent so you can tell if he wins the board or not at this play
+                    return this.botPlay;
+                }
+            }
+            if (isMiddleAvailableForCatching(boardPlay)) {
+                if (otherPlayerWinSBoardNextTurn()) {
+                    // Mark the position as not allowing the opponent to win after catching the middle
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 50;
+                } else if (otherPlayerCanPlayAnywhere()) {
+                    // Mark the position as available for the opponent to play after catching the middle
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 10;
+                } else {  // The numbers are different, so you can tell if he wins the board or not at this play
+                    return this.botPlay;
+                }
+            }
+            while (canCatchRandomCorner(boardPlay)) {
+                if (otherPlayerWinSBoardNextTurn()) {
+                    // Mark the position as available for the opponent to play after catching a random corner
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 10;
+                } else if (otherPlayerCanPlayAnywhere()) {
+                    // Mark the position as available for the opponent to play after catching a random corner
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 10;
+                } else {
+
+                    return this.botPlay;
+                }
+            }
+            while (canCatchRandomMiddleSide(boardPlay)) {
+                if (otherPlayerWinSBoardNextTurn()) {
+                    // Mark the position as available for the opponent to play after catching a random middle side
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 10;
+                } else if (otherPlayerCanPlayAnywhere()) {
+                    // Mark the position as available for the opponent to play after catching a random middle side
+                    this.playPossibility[this.botPlay / 10][this.botPlay % 10] = 10;
+                } else {
+                    return this.botPlay;
+                }
+            }
+            // Check available positions for the bot to play
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (this.playPossibility[i][j] == 10) { //first option to play no matter what
+                        this.botPlay = i * 10 + j;
+                        break;
+                    }
+                }
+            }
+
+            // Check positions where the bot can win the small board, but the player next turn can win a small board or play anywhere
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (this.playPossibility[i][j] == 100) { //win the small board but the player next turn can win a small board or he can play any where
+                        this.botPlay = i * 10 + j;
+                        break;
+                    } else if (this.playPossibility[i][j] == 50) { //catch middle but the player next turn can win a small board or he can play any where
+                        this.botPlay = i * 10 + j;
+                    }
+                }
+            }
+            return this.botPlay;
+        }
+
+    }
+
+    /**
+     * Check if the bot can win the current small board in the specified position.
+     *
+     * @param boardPlay The position of the small board where the bot is making a move.
+     * @return True if the bot can win the small board on the next move, false otherwise.
+     */
+    public Boolean canWinSBoard(int boardPlay) {
+        return canWinNextMoveSmallBoard(this.gb.getSmallBoard()[boardPlay / 10][boardPlay % 10], this.botNum);
+    }
+
+    /**
+     * Check if the bot can win the big board.
+     *
+     * @return True if the bot can win the big board on the next move, false otherwise.
+     */
+    public Boolean canWinBBoard() {
+        return canWinNextMoveBigBoard(this.gb.getBigBoard(), this.botNum);
+    }
+
+    /**
+     * Check if the other player can win the current small board on the next turn.
+     *
+     * @return True if the other player can win the small board on the next turn, false otherwise.
+     */
+    public Boolean otherPlayerWinSBoardNextTurn() {
+        if (this.botNum == 1) {
+            return canWinNextMoveSmallBoard(this.gb.getSmallBoard()[this.botPlay / 10][this.botPlay % 10], -1);
+        } else if (this.botNum == -1) {
+            return canWinNextMoveSmallBoard(this.gb.getSmallBoard()[this.botPlay / 10][this.botPlay % 10], 1);
+        } else {
+            //error
+            return null;
+        }
+    }
+
+    /**
+     * Check if the other player can play anywhere on the current big board.
+     *
+     * @return True if the other player can play anywhere on the big board, false otherwise.
+     */
+    public Boolean otherPlayerCanPlayAnywhere() {
+        if (gb.getBigBoard()[this.botPlay / 10][this.botPlay % 10] == 0) {
+            return false;
+        } else if (gb.getBigBoard()[this.botPlay / 10][this.botPlay % 10] == 1 || gb.getBigBoard()[this.botPlay / 10][this.botPlay % 10] == -1) {
+            return true;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Check if the middle position is available for catching on the current small board.
+     *
+     * @param boardPlay The position of the small board where the bot is making a move.
+     * @return True if the middle position is available for catching, false otherwise.
+     */
+    public Boolean isMiddleAvailableForCatching(int boardPlay) {
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][1] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][1] == -1 || playPossibility[1][1] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][1] == 0) {
+            return true;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Check if the bot can catch a random corner on the current small board.
+     *
+     * @param boardPlay The position of the small board where the bot is making a move.
+     * @return True if the bot can catch a random corner, false otherwise.
+     */
+    public Boolean canCatchRandomCorner(int boardPlay) {
+        //#1
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][0] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][0] == -1 || playPossibility[0][0] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][0] == 0) {
+            return true;
+        }
+
+        //#2
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][2] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][2] == -1 || playPossibility[0][2] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][2] == 0) {
+            return true;
+        }
+
+        //#3
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][0] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][0] == -1 || playPossibility[2][0] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][0] == 0) {
+            return true;
+        }
+
+        //#4
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][2] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][2] == -1 || playPossibility[2][2] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][2] == 0) {
+            return true;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Check if the bot can catch a random middle side on the current small board.
+     *
+     * @param boardPlay The position of the small board where the bot is making a move.
+     * @return True if the bot can catch a random middle side, false otherwise.
+     */
+    public Boolean canCatchRandomMiddleSide(int boardPlay) {
+        //#1
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][1] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][1] == -1 || playPossibility[0][1] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][0][1] == 0) {
+            return true;
+        }
+
+        //#2
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][2] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][2] == -1 || playPossibility[1][2] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][2] == 0) {
+            return true;
+        }
+
+        //#3
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][1] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][1] == -1 || playPossibility[2][1] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][2][1] == 0) {
+            return true;
+        }
+
+        //#4
+        if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][0] == 1 || gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][0] == -1 || playPossibility[1][0] != 0) {
+            return false;
+        } else if (gb.getSmallBoard()[boardPlay / 10][boardPlay % 10][1][0] == 0) {
+            return true;
+        } else {
+            return null;
+        }
+    }
+
+
+    /**
+     * Checks if the given player can win on the next move in a small tic-tac-toe board.
+     *
+     * @param board  The small tic-tac-toe board to check.
+     * @param player The player (X or O) for whom to check the winning possibility.
+     * @return true if the player can win on the next move, false otherwise.
+     */
+    public boolean canWinNextMoveSmallBoard(int[][] board, int player) {
+        // Check each empty spot on the board
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                this.playPossibility[i][j] = true;
+                if (board[i][j] == 0) {
+                    // Make the move and check if it results in a win
+                    board[i][j] = player;
+                    if (this.gb.WinCheck(board, player)) {
+                        // Undo the move
+                        this.botPlay = i * 10 + j;
+                        board[i][j] = 0;
+                        return true; // Player can win on the next move
+                    }
+                    // Undo the move
+                    board[i][j] = 0;
+                }
             }
         }
-        play = -1;
 
-        if (canWinSBoard()) {
-            if (canWinBBoard()) {
-                // Implement logic to win the big board
-            }
-            if (otherPlayerWinSBoardNextTurn()) {
-                // Implement logic to prevent the other player from winning the small board
-            }
-            if (otherPlayerCanPlayAnywhere()) {
-                // Implement logic to handle when the other player can play anywhere
-            } else {
-                return this.play;
+        return false; // Player cannot win on the next move
+    }
+
+    /**
+     * Checks if the given player can win on the next move in a big tic-tac-toe board.
+     *
+     * @param board  The big tic-tac-toe board to check.
+     * @param player The player (X or O) for whom to check the winning possibility.
+     * @return true if the player can win on the next move, false otherwise.
+     */
+    public boolean canWinNextMoveBigBoard(int[][] board, int player) {
+        // Check each empty spot on the board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {
+                    // Make the move and check if it results in a win
+                    board[i][j] = player;
+                    if (this.gb.WinCheck(board, player)) {
+                        // Undo the move
+                        board[i][j] = 0;
+                        return true; // Player can win on the next move
+                    }
+                    // Undo the move
+                    board[i][j] = 0;
+                }
             }
         }
-        if (canCatchMiddle()) {
-            if (canWinBBoard()) {
-                // Implement logic to win the big board
-            }
-            if (otherPlayerWinSBoardNextTurn()) {
-                // Implement logic to prevent the other player from winning the small board
-            }
-            if (otherPlayerCanPlayAnywhere()) {
-                // Implement logic to handle when the other player can play anywhere
-            } else {
-                return this.play;
-            }
-        }
-        while (canCatchRandomCorner()) {
-            if (canWinBBoard()) {
-                // Implement logic to win the big board
-            }
-            if (otherPlayerWinSBoardNextTurn()) {
-                // Implement logic to prevent the other player from winning the small board
-            }
-            if (otherPlayerCanPlayAnywhere()) {
-                // Implement logic to handle when the other player can play anywhere
-            } else {
-                return this.play;
-            }
-        }
-        while (canCatchRandomMiddleSide()) {
-            if (canWinBBoard()) {
-                // Implement logic to win the big board
-            }
-            if (otherPlayerWinSBoardNextTurn()) {
-                // Implement logic to prevent the other player from winning the small board
-            }
-            if (otherPlayerCanPlayAnywhere()) {
-                // Implement logic to handle when the other player can play anywhere
-            } else {
-                return this.play;
-            }
-        }
-        // Implement a default behavior if none of the conditions are met
-        // You may return a random move or apply some strategy here.
 
-        return this.play;
-    }
-
-    public Boolean canWinSBoard() {
-        // Implement logic to check if the bot can win a small board
-    }
-
-    public Boolean canWinBBoard() {
-        // Implement logic to check if the bot can win the big board
-    }
-
-    public Boolean otherPlayerWinSBoardNextTurn() {
-        // Implement logic to check if the other player can win a small board next turn
-    }
-
-    public Boolean otherPlayerCanPlayAnywhere() {
-        // Implement logic to check if the other player can play anywhere
-    }
-
-    public Boolean canCatchMiddle() {
-        // Implement logic to check if the bot can catch the middle
-    }
-
-    public Boolean canCatchRandomCorner() {
-        // Implement logic to check if the bot can catch a random corner
-    }
-
-    public Boolean canCatchRandomMiddleSide() {
-        // Implement logic to check if the bot can catch a random middle side
+        return false; // Player cannot win on the next move
     }
 }
